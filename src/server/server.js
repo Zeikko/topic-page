c5a00fc74b3b0fb7bc7c9201770791d5826d21db
Jaakko Ojalehto <jaakko@ojalehto.fi>
2015-09-10 21:03:41 +0300

    Add header and footer

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,8 @@ import request from 'superagent'
 import swig from 'swig'
 import moment from 'moment'
 import _ from 'lodash'
+import async from 'async'
+
 let app = express()
 
 app.engine('html', swig.renderFile);
@@ -14,24 +16,21 @@ swig.setDefaults({
 });
 app.use(express.static('src/public'));
 
-app.get('/api/:keyword', function(req, res) {
-  request
-    .get('http://haku.yle.fi/api/search')
-    .query({
-      q: req.params.keyword,
-      keyword: req.params.keyword
+app.get('/:keyword', (req, res) => {
+  async.parallel({
+    header: getHeader,
+    footer: getFooter,
+    results: (callback) => {
+      getResults(req.params.keyword, callback)
+    }
+  }, (err, data) => {
+    res.render('index', {
+      header: data.header.text,
+      footer: data.footer.text,
+      keyword: req.params.keyword,
+      results: data.results
     })
-    .set('Accept', 'application/json')
-    .end(function(err, response) {
-      let results = _.map(response.body.results, (result) => {
-        result.date = moment(result.date).format('D.M.YYYY')
-        return result
-      })
-      res.render('index', {
-        keyword: req.params.keyword,
-        results: results
-      })
-    })
+  });
 })
 
 let server = app.listen(process.env.PORT || 3000, () => {
@@ -40,3 +39,32 @@ let server = app.listen(process.env.PORT || 3000, () => {
 
   console.log('Example app listening at http://%s:%s', host, port)
 });
+
+let getHeader = (callback) => {
+  request
+    .get('http://yle.fi/global/api/load.php?version=5&modules=html/header')
+    .end(callback)
+}
+
+let getFooter = (callback) => {
+  request
+    .get('http://yle.fi/global/api/load.php?version=5&modules=html/footer')
+    .end(callback)
+}
+
+let getResults = (keyword, callback) => {
+  request
+    .get('http://haku.yle.fi/api/search')
+    .query({
+      q: keyword,
+      keyword: keyword
+    })
+    .set('Accept', 'application/json')
+    .end((err, response) => {
+      let results = _.map(response.body.results, (result) => {
+        result.date = moment(result.date).format('D.M.YYYY')
+        return result
+      })
+      callback(err, results)
+    })
+}
